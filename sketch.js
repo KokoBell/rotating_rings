dcea7e079e3a0f0dcbcc00859c5a674c2b1a2eb2
Tumelo Koko <tumelokoko10@gmail.com>
2022-09-24 22:30:24 +0200

    Feature: Make the rings react to mouse movement

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ let seed = []
 let d1, d2, d3
 
 function setup() {
-    createCanvas(window.innerWidth*0.98, window.innerHeight, WEBGL)
+    createCanvas(window.innerWidth*0.98, window.innerHeight*0.96, WEBGL)
     noStroke()
     switchCircles()
 }
@@ -33,8 +33,8 @@ function makeTorus() {
     for (let i = 0; i < toruses; i++) {
         push()
         fill(seed[i])
-        rotateX(d1 * angle * 0.09 * i * velocity)
-        rotateY(d2 * angle * 0.09 * i * velocity)
+        rotateX(d1 * angle * 0.09 * i * velocity+mouseX*0.001)
+        rotateY(d2 * angle * 0.09 * i * velocity+mouseY*0.001)
         rotateZ(d3 * angle * 0.09 * i * velocity)
         torus(50 * i, 10 * i * thickness / 2)
         pop()
